eb3bf7dabf3810227c1acd620d7eced9a96bc69c
gqxjones <1186835847@qq.com>
2020-03-26 21:47:45 +0800

    pytest优化

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -5,65 +5,42 @@
 pytest的基本使用
 '''
 
+import pytest
 from python.Calc import Calc
 
+
 class TestCalc:
-    def test_addOne(self):
-        calc = Calc()
-        assert calc.add(0, 2) == 2
 
-    def test_addTwo(self):
-        calc = Calc()
-        assert calc.add(1, 2) == 3
+    def setup(self):
+        print("setup")
+        self.Calc = Calc()
 
-    def test_addThree(self):
-        calc = Calc()
-        assert calc.add(-2, 1) == -1
+    @pytest.mark.parametrize('result, a, b', [
+        [2, 0, 2],
+        [3, 1, 2],
+        [0, 2, 2],
+        [-2, -3, -5],
+        [1, 3, 4]
+    ])
+    #加法
+    # @pytest.mark.run(order=-1)
+    def test_zaddOne(self, result, a, b):
+        # assert self.Calc.add(0, 2) == 2
+        assert self.Calc.add(a, b) == result
 
-    def test_addFour(self):
-        calc = Calc()
-        assert calc.add(2, -1) == 1
+    # @pytest.mark.run(order=1)
+    # def test_addTwo(self):
+    #     assert self.Calc.add(1, 2) == 3
 
-    def test_addFive(self):
-        calc = Calc()
-        assert calc.add(2, 0) == 2
-
-    def test_addSix(self):
-        calc = Calc()
-        assert calc.add(0.01, 2) == 2.01
-
-    def test_addSeven(self):
-        calc = Calc()
-        assert calc.add(1, 2) != 2
-
+    @pytest.mark.parametrize('divResult, c, d', [
+        [0.5, 1, 2],
+        [2, 2, 1],
+        [2, 0.2, 0.1]
+    ])
     #除法
-    def test_divOne(self):
-        calc = Calc()
-        assert calc.div(0, 2) == 0
+    def test_divOne(self, divResult, c, d):
+        assert self.Calc.div(c, d) == divResult
 
     def test_divTwo(self):
-        calc = Calc()
-        assert calc.div(1, 10) == 0.1
-
-    def test_divThree(self):
-        calc = Calc()
-        assert calc.div(-2, 1) == -2
-
-    def test_divFour(self):
-        calc = Calc()
-        assert calc.div(2, -1) == -2
-
-    def test_divFive(self):
-        calc = Calc()
-        assert calc.div(5.5, 5) == 1.1
-
-    def test_divSix(self):
-        calc = Calc()
-        try:
-            calc.div(2, 0)
-        except Exception as e:
-            print(e)
-
-    def test_divSeven(self):
-        calc = Calc()
-        assert calc.div(2, 2) != 2
+        with pytest.raises(ZeroDivisionError):
+            self.Calc.div(2, 0)
